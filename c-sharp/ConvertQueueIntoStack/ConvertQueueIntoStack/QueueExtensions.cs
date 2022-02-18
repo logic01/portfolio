540b5456f66bc6ee979fd1a0c5bf9fc0dbcd6805
logic01 <abstractlogic01@gmail.com>
2022-02-18 14:13:19 -0700

    Convert Queue Into Stack

---
+++ c-sharp/ConvertQueueIntoStack/ConvertQueueIntoStack/QueueExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace ConvertQueueIntoStack
+{
+    public static class QueueExtensions
+    {
+        public static Stack<int> ToStack(this Queue<int> queue)
+        {
+            // queue FIFO - 
+            // A
+            // BA
+            // CBA
+            // PRINT: ABC
+
+            // stack LIFO - 1, 2, 3  -> 1, 2, 3
+            // A
+            // BA
+            // CBA
+            // PRINT: CBA
+
+            // cheat way - Show your work!
+            // Stack<int> stack = new(queue);
+            // Stack<int> stack2 = new(stack);
+
+            Stack<int> stack = new();
+            Stack<int> stack2 = new();
+
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+
+            while (stack.Count > 0)
+            {
+                stack2.Push(stack.Pop());
+            }
+
+            return stack2;
+        }
+    }
+}
