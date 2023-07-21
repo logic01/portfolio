81d5ec7031dd442df7c9904433cfd7db9e135057
Brandon Pohl <Brandon.Pohl@transunion.com>
2023-07-21 10:34:06 -0600

    New leetcode

---
+++ leetcode/c-sharp/LeetCode.Wave.Two/ReverseLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.Wave.Two.Util;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Wave.Two
+{
+    /// <summary>
+    /// Given a singly linked list, reverse it in place, that is, without using additional memory
+    /// </summary>
+    public static class ReverseLinkedList
+    {
+        public static Node? Do(Node? root)
+        {
+            if (root == null)
+                return root;
+
+            if (root.next == null)
+                return root;
+
+            Node? cur = root;
+            Node? prev = null;
+
+            // end if we hit a cycle
+            while (cur != null)
+            {
+                var next = cur.next;
+
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+
+                // end at cycle
+                if (cur == root)
+                {
+                    // reset cycle
+                    prev.next = cur;
+                    break;
+                }
+            }
+
+            // this will be the last good node before we hit null end
+            return prev;
+        }
+    }
+}
---
+++ leetcode/c-sharp/LeetCode.Wave.Two/TreeBFS.cs
@@ -0,0 +1,37 @@
+﻿
+using LeetCode.Wave.Two.Util;
+
+namespace LeetCode.Wave.Two
+{
+    public static class TreeBFS
+    {
+        public static TreeNode? Do(TreeNode? root, int val)
+        {
+            if (root == null)
+                return root;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                if (current.Value == val)
+                {
+                    Console.WriteLine("Found");
+                    return current;
+                }
+
+                if (current.Left != null)
+                    queue.Enqueue(current.Left);
+
+                if (current.Right != null)
+                    queue.Enqueue(current.Right);
+            }
+
+            Console.WriteLine("Not Found");
+            return null;
+        }
+    }
+}
